712072ba58b9745e20c5d59fa0400ae10451dd7f
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-25 09:40:17 -0300

    :sparkles: feat: Adicionado Rotas em Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -35,10 +35,12 @@ function Navbar() {
           </div>
 
           <div className="flex items-center gap-4 py-4">
+            <Link to="/categorias" className="hover:underline">
               Categorias
-              
+            </Link>
+            <Link to="/cadastrarcategoria" className="hover:underline">
               Cadastrar Categoria
-           
+            </Link>
             <User size={32} weight="bold" />
 
             <ShoppingCart size={32} weight="bold" />
