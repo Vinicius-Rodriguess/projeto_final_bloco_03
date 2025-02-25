cfe0976372eb88e0c9af5d83ac7e2899c201df89
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-25 09:50:50 -0300

    :sparkles: feat: Adicionado CRUD de Produto

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import Footer from "./components/footer/Footer";
 import ListarCategorias from "./components/categorias/listarcategorias/ListarCategorias";
 import FormCategoria from "./components/categorias/formcategoria/FormCategoria";
 import DeletarCategoria from "./components/categorias/deletarcategorias/DeletarCategoria";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import ListarProdutos from "./components/produtos/listarprodutos/ListarProdutos";
+import FormProduto from "./components/produtos/formproduto/FormProduto";
+import DeletarProduto from "./components/produtos/deletarprodutos/DeletarProduto";
 
 function App() {
   return (
     <>
-    <ToastContainer />
+      <ToastContainer />
       <BrowserRouter>
         <Navbar />
         <div className="flex flex-col min-h-screen bg-gray-200 ">
@@ -24,6 +27,10 @@ function App() {
               path="/deletarcategoria/:id"
               element={<DeletarCategoria />}
             />
+            <Route path="/produtos" element={<ListarProdutos />} />
+            <Route path="/cadastrarproduto" element={<FormProduto />} />
+            <Route path="/editarproduto/:id" element={<FormProduto />} />
+            <Route path="/deletarproduto/:id" element={<DeletarProduto />} />
           </Routes>
         </div>
         <Footer />
